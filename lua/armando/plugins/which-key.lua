22d7c0bf01dc9c67071834cdeacc121c126f75bd
LavoroPulito <100956316+LavoroPulito@users.noreply.github.com>
2026-05-08 16:11:55 +0200

    modern which-key

--- lua/armando/plugins/which-key.lua
+++ lua/armando/plugins/which-key.lua
@@ -6,8 +6,18 @@ return {
     vim.o.timeoutlen = 500
   end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+    -- Il preset "modern" è quello più simile allo stile di Noice
+    preset = "modern",
+    win = {
+      border = "rounded", -- Bordi arrotondati come i popup di Noice
+      -- Nella v3 'position' è gestita diversamente, 
+      -- per averlo in basso basta non specificare 'anchor' o usare i default
+      no_overlap = true,   -- Evita che il popup copra la riga di comando
+      padding = { 1, 2 },  -- Un po' di respiro interno
+    },
+    -- Se vuoi che appaia più simile a una finestra di Noice
+    layout = {
+      spacing = 3, -- Spazio tra le colonne
+      align = "left",
+    }, },
 }
